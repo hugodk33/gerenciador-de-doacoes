021d324812005b6bf85a2538abb489a2e438f79f
Victor Hugo A. Arruda <victor.arruda@mobwire.com.br>
2023-06-16 08:22:23 -0300

    fixxx

--- src/app/lista-de-atendimentos/page.tsx
+++ src/app/lista-de-atendimentos/page.tsx
@@ -3,6 +3,9 @@ import SideMenu from '@/components/sections/sidemenu'
 import Content from '@/components/sections/content';
 import Header from '@/components/sections/header';
 
+import { AiOutlineEye } from 'react-icons/ai'
+import { BsFillTrashFill , BsEye } from 'react-icons/bs';
+
 export default function ListaDeAtendimentos() {
   return (
     <section>
@@ -12,28 +15,89 @@ export default function ListaDeAtendimentos() {
           <SideMenu />
           <Content>
             <h1 className='text-3xl pb-2 pt-2'>Atendimentos</h1>
-            <hr />
-            <ul className="flex flex-col p-4 gap-3 rounded bg-gray-200 overflow-y-scroll mb-5">
+            
+            <ul className="flex flex-col p-4 gap-3 rounded overflow-y-scroll mb-5">
               
               <li key={'key-bottom-'} className="flex flex-row text-4xl">
+                <button className="flex rounded-full justify-center bg-gray-100 drop-shadow-md w-10 h-10 text-gray mr-5" >
+                  <BsFillTrashFill style={{fontSize: '24px' , marginTop: '8px'}}/>
+                </button>
                 <button className="flex rounded-full justify-center bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 drop-shadow-md w-10 h-10 text-gray mr-5" >
-
+                  <BsEye className='text-white' style={{fontSize: '24px' , marginTop: '8px'}}/>
                 </button>
                 <div className="">
-                  <p className='text-2xl'>{'a.nome'}</p>
-                  <p className='text-sm uppercase font-semibold'>lorem ipsum</p>
+                  <p className='text-2xl'>Pessoas em situação de risco em local tal</p>
+                  <p className='text-sm uppercase font-semibold'>11/11/11 - 2 pessoas em uma casa - <b className='text-green-500'>DESPACHADO</b></p>
                 </div>
               </li>
+              <hr />
+              <li key={'key-bottom-'} className="flex flex-row text-4xl">
+                <button className="flex rounded-full justify-center bg-gray-100 drop-shadow-md w-10 h-10 text-gray mr-5" >
+                  <BsFillTrashFill style={{fontSize: '24px' , marginTop: '8px'}}/>
+                </button>
+                <button className="flex rounded-full justify-center bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 drop-shadow-md w-10 h-10 text-gray mr-5" >
+                  <BsEye className='text-white' style={{fontSize: '24px' , marginTop: '8px'}}/>
+                </button>
+                <div className="">
+                  <p className='text-2xl'>Família precisando fazer festa de 15 anos</p>
+                  <p className='text-sm uppercase font-semibold'>11/11/11 - 1 pessoas em uma casa - <b className='text-red-500'>PENDENTE</b></p>
+                </div>
+              </li>
+              <hr />
 
               <li key={'key-bottom-'} className="flex flex-row text-4xl">
+                <button className="flex rounded-full justify-center bg-gray-100 drop-shadow-md w-10 h-10 text-gray mr-5" >
+                  <BsFillTrashFill style={{fontSize: '24px' , marginTop: '8px'}}/>
+                </button>
                 <button className="flex rounded-full justify-center bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 drop-shadow-md w-10 h-10 text-gray mr-5" >
-
+                  <BsEye className='text-white' style={{fontSize: '24px' , marginTop: '8px'}}/>
                 </button>
                 <div className="">
-                  <p className='text-2xl'>{'a.nome'}</p>
-                  <p className='text-sm uppercase font-semibold'>lorem ipsum</p>
+                  <p className='text-2xl'>Pessoas em situação de risco em local tal</p>
+                  <p className='text-sm uppercase font-semibold'>11/11/11 - 2 pessoas em uma casa - <b className='text-green-500'>DESPACHADO</b></p>
                 </div>
               </li>
+              <hr />
+              <li key={'key-bottom-'} className="flex flex-row text-4xl">
+                <button className="flex rounded-full justify-center bg-gray-100 drop-shadow-md w-10 h-10 text-gray mr-5" >
+                  <BsFillTrashFill style={{fontSize: '24px' , marginTop: '8px'}}/>
+                </button>
+                <button className="flex rounded-full justify-center bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 drop-shadow-md w-10 h-10 text-gray mr-5" >
+                  <BsEye className='text-white' style={{fontSize: '24px' , marginTop: '8px'}}/>
+                </button>
+                <div className="">
+                  <p className='text-2xl'>Família precisando fazer festa de 15 anos</p>
+                  <p className='text-sm uppercase font-semibold'>11/11/11 - 1 pessoas em uma casa - <b className='text-red-500'>PENDENTE</b></p>
+                </div>
+              </li>
+              <hr />
+
+              <li key={'key-bottom-'} className="flex flex-row text-4xl">
+                <button className="flex rounded-full justify-center bg-gray-100 drop-shadow-md w-10 h-10 text-gray mr-5" >
+                  <BsFillTrashFill style={{fontSize: '24px' , marginTop: '8px'}}/>
+                </button>
+                <button className="flex rounded-full justify-center bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 drop-shadow-md w-10 h-10 text-gray mr-5" >
+                  <BsEye className='text-white' style={{fontSize: '24px' , marginTop: '8px'}}/>
+                </button>
+                <div className="">
+                  <p className='text-2xl'>Pessoas em situação de risco em local tal</p>
+                  <p className='text-sm uppercase font-semibold'>11/11/11 - 2 pessoas em uma casa - <b className='text-green-500'>DESPACHADO</b></p>
+                </div>
+              </li>
+              <hr />
+              <li key={'key-bottom-'} className="flex flex-row text-4xl">
+                <button className="flex rounded-full justify-center bg-gray-100 drop-shadow-md w-10 h-10 text-gray mr-5" >
+                  <BsFillTrashFill style={{fontSize: '24px' , marginTop: '8px'}}/>
+                </button>
+                <button className="flex rounded-full justify-center bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 drop-shadow-md w-10 h-10 text-gray mr-5" >
+                  <BsEye className='text-white' style={{fontSize: '24px' , marginTop: '8px'}}/>
+                </button>
+                <div className="">
+                  <p className='text-2xl'>Família precisando fazer festa de 15 anos</p>
+                  <p className='text-sm uppercase font-semibold'>11/11/11 - 1 pessoas em uma casa - <b className='text-red-500'>PENDENTE</b></p>
+                </div>
+              </li>
+              <hr />
               
             </ul>
           </Content>
